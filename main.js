ac9302890c2b5fd9ffcb35b2243ee478a55fb8d5
unknown <arthurmatosvesiani@gmail.com>
2024-09-16 11:16:48 -0300

    base de dados

---
+++ main.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    tipo: "Educação",
+    tarefa: "Fazer a tarefa de casa",
+    status: false,
+  },
+  {
+    tipo: "Esporte",
+    tarefa: "Fazer caminhada",
+    status: true,
+  },
+  {
+    tipo: "Esporte",
+    tarefa: "Jogar golf",
+    status: false,
+  },
+];
